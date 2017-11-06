2cc78bef699adf7f22db40d4ad476d4592e8944b
Сергей Попол <popolsergey@gmail.com>
2017-11-06 21:36:50 +0100

    Initial code

---
+++ config/dev.exs
@@ -0,0 +1,7 @@
+use Mix.Config
+
+config :apisex_basicauth,
+  clients: [
+    {"client_id1", "client_secret1"},
+    {"client_id2", "client_secret2"}
+  ]
---
+++ config/test.exs
@@ -0,0 +1,5 @@
+config :apisex_basicauth,
+  [
+    {"client_id1", "client_secret1"},
+    {"client_id2", "client_secret2"}
+  ]
