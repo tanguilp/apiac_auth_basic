0e0de496006de88e18be685f47d81d33f65e0b4f
Сергей Попол <popolsergey@gmail.com>
2017-11-08 21:42:52 +0100

    Configuration type and structure

---
+++ lib/apisex_basicauth_config.ex
@@ -0,0 +1,23 @@
+defmodule APISexBasicAuthConfig do
+  @type t :: %__MODULE__{clients: list({String.t, String.t}), callback: nil | (String.t, String.t -> String.t), advertise_wwwauthenticate_header: boolean(), realm: String.t}
+
+  defstruct clients: [],
+            callback: nil,
+            advertise_wwwauthenticate_header: true,
+            realm: "Default realm"
+
+  def init({app, configkey}) do
+    conf = Application.get_env(app, configkey)
+    %__MODULE__{
+      clients: Keyword.get(conf, :clients, []),
+      callback: Keyword.get(conf, :callback),
+      advertise_wwwauthenticate_header:
+        Keyword.get(conf, :advertise_wwwauthenticate_header, true),
+      realm: Keyword.get(conf, :realm)
+    }
+  end
+
+  def init(conf) when is_list(conf) do
+    struct(__MODULE__, conf)
+  end
+end
